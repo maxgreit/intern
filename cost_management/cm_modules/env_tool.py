1c9f89b75b5875357462c075ce7163126032e90a
mgsrood <mgsrood@gmail.com>
2025-01-10 14:19:24 +0100

    Changed env_tool

--- cost_management/cm_modules/env_tool.py
+++ cost_management/cm_modules/env_tool.py
@@ -2,8 +2,17 @@ from dotenv import load_dotenv
 import logging
 import os
 
+def determine_base_dir():
+    if "maxrood" in os.path.expanduser("~"):  # Specifiek voor je MacBook
+            return "/Users/maxrood/werk/greit/greit_scripts"
+    else:  # Voor je VM
+        return "/home/greit/klanten/intern/"
+
 def env_check():
-    if os.path.exists('.env'):
+    base_dir = determine_base_dir()
+    env_path = os.path.join(base_dir, '.env')
+    
+    if os.path.exists(env_path):
             load_dotenv()
             print("Lokaal draaien: .env bestand gevonden en geladen.")
             logging.info("Lokaal draaien: .env bestand gevonden en geladen.")
