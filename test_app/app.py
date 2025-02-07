9fa2578e3c4696a0ec044b718a432ff70b494989
mgsrood <mgsrood@gmail.com>
2025-02-07 15:24:02 +0100

    Debug 2

--- test_app/app.py
+++ test_app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from modules.env_tool import env_check
 from woocommerce import API
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/abonnementen/*": {"origins": "*"}})
 
 # Check uitvoering: lokaal of productie
 env_check()
@@ -28,4 +30,4 @@ def get_subscriptions(email):
     return jsonify({"email": email})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8443)
+    app.run(host='0.0.0.0', port=5000) 
